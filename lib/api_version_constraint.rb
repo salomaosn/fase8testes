ead0b2a7c2d021eaf61adcb0727f6bd2c806dc9d
salomaosn <salomao.sgmogi@gmail.com>
2020-02-28 14:08:51 +0000

    Testes finalizados

---
+++ lib/api_version_constraint.rb
@@ -0,0 +1,11 @@
+class ApiVersionConstraint
+    def initialize(options)
+        @version = options[:version]
+        @default = options[:version]
+    end
+    
+    def matches?(req)
+        @default || req.headers['Accept']
+            .include?("application/vnd.projetofase8.v#{@version}")
+    end
+end
