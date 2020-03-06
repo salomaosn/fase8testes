16e24c742d902d0fa7ff1dfabfa0d832bfb59b7a
salomaosn <salomao.sgmogi@gmail.com>
2020-03-06 13:12:52 +0000

    Testes finalizados

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,39 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Users API', type: :request do
-    let!(:user) { create(:user) }
-    let(:user_id) { user.id }
-    
-    before { host! "localhost:3000/api" }
-    
-    describe "GET user/:id" do
-        before do
-           headers = { "Accept" => "aplication/vnd.projetofase8.v1" }
-           get "/users/#{user_id}", params: {}, headers: headers
-        end
-        
-        context "when the user exists" do
-            it "returns the user" do
-                user_response = JSON.parse(response.body)
-                expect(user_response["id"]).to eq(user_id)
-            end
-            it "returns status code 200" do
-                expect(response).to have_http_status(200)
-            end
-        end
-        
-        context "when the user does not exist" do
-            let(:user_id){ 10000 }
-            
-            it "returns status code 404" do
-               expect(response).to have_http_status(404) 
-            end 
-        end
-    end
+  let!(:user) { create(:user) }
+  let(:user_id) { user.id }
+  let(:headers) { { "Accept" => "aplication/vnd.projetofase8.v1" }}
+
+  before { host! "localhost:3000/api" }
+  
+  describe "GET user/:id" do
+      before do
+          get "/users/#{user_id}", params: {}, headers: headers
+      end
+      
+      context "when the user exists" do
+          it "returns the user" do
+              expect(json_body["id"]).to eq(user_id)
+          end
+          it "returns status code 200" do
+              expect(response).to have_http_status(200)
+          end
+      end
+      
+      context "when the user does not exist" do
+          let(:user_id){ 10000 }
+          
+          it "returns status code 404" do
+              expect(response).to have_http_status(404) 
+          end 
+      end
+  end
 
   describe "POST user/" do
     before do
-      headers = { "Accept" => "application/vnd.projetofase8.v1" }
       post "/users/", params: { user: user_params }, headers: headers
     end
     
@@ -45,8 +43,7 @@ RSpec.describe 'Users API', type: :request do
       end
       
       it "returns json data for the created user" do
-        user_response = JSON.parse(response.body)
-        expect(user_response['email']).to eq(user_params[:email])
+        expect(json_body['email']).to eq(user_params[:email])
       end
     end
     
@@ -58,9 +55,52 @@ RSpec.describe 'Users API', type: :request do
       end
       
       it "returns the json data for the errors" do
-        user_response = JSON.parse(response.body)
-        expect(user_response).to have_key('errors')
+        expect(json_body).to have_key('errors')
       end
+    end
+  end
+
+  describe "PUT user/:id" do
+    before do
+       put "/users/#{user_id}", params: { user: user_params }, headers: headers 
+    end
+    
+    context "when the request params are valid" do
+       let(:user_params){ { email: 'novo@email.com' } }
+       
+       it "returns status code 200" do
+          expect(response).to have_http_status(200)
+       end
+       
+       it "returns json data for the update user" do
+          expect(json_body['email']).to eq(user_params[:email])
+       end
+    end
+    
+    context "when the request params are invalid" do
+       let(:user_params){ { email: 'email_invalid@' } }
+       
+       it "returns status code 422" do
+          expect(response).to have_http_status(422)
+       end
+       
+       it "returns the json data for the errors" do
+          expect(json_body).to have_key('errors')
+       end
+    end
+  end
+
+  describe "DELETE user/:id" do
+    before do
+        delete "/users/#{user_id}", params: {}, headers: headers
+    end
+    
+    it "returns status code 204" do
+        expect(response).to have_http_status(204)
+    end
+    
+    it "removes the user from database" do
+        expect( User.find_by(id: user.id) ).to be_nil
     end
   end
 end
